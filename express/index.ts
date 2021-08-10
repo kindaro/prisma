44b316f5fc434fcd0b5015d238fdffee6b52ddde
Ignat Insarov <kindaro@gmail.com>
2021-08-10 14:41:15 +0500

    Get a generated server running.

---
+++ express/index.ts
@@ -0,0 +1,31 @@
+import express from 'express'
+import getopts from 'getopts'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+const app = express()
+
+/* Post */
+app.use (express.json ({strict: false}))
+app.get('/post', async function(req, res, next) {
+	  res.status(200).send(await prisma.post.findMany ( ))
+})
+app.post('/post', async function(req, res, next) {
+    await prisma.post.create ({data: {content: req.body}})
+	  res.status(200).send( )
+})
+
+const options = getopts(process.argv.slice(2), {
+	alias: {
+		port: 'p',
+	},
+	unknown: (option) => {
+		console.log(`Unknown option: ${option}`)
+		return false
+	},
+})
+
+const port = options.port || 3000
+const server = app.listen(port, function() {
+	console.log(`Listening on ${port}`)
+})
